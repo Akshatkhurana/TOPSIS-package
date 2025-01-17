428e2a4dfa2d95842a975eff21653404cecd413c
Akshat Khurana <khuranaakshat33@gmail.com>
2025-01-17 23:53:29 +0530

    Stop tracking images folder and added it to .gitignore

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="topsis_Akshat_102203644",
+    packages=find_packages(),
     version="0.1.0",       
     author="Akshat Khurana",  # Your name
     author_email="akhurana_be22@thapar.edu",  # Your email
     description="A Python package to perform TOPSIS (Technique for Order Preference by Similarity to Ideal Solution).",
     long_description=open("README.md").read(),  # Make sure you have a README.md
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/topsis-akshat",  # Replace with your GitHub repo URL
-    packages=find_packages(),  # Automatically find packages in the folder
+    url="https://github.com/Akshatkhurana/TOPSIS-package",  # Replace with your GitHub repo URL
+      # Automatically find packages in the folder
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -17,6 +18,7 @@ setup(
     ],
     python_requires=">=3.6",  # Specify Python version compatibility
     install_requires=[
-        "numpy",  # Add dependencies here (e.g., pandas, numpy, etc.)
+        "numpy",
+        "pandas" # Add dependencies here (e.g., pandas, numpy, etc.)
     ],
 )
